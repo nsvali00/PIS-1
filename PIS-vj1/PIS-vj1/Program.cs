c02b6a7763cb8938876065b46387bd49ce0d123e
atodoric <toda2992@gmail.com>
2016-03-08 08:57:56 +0100

    kommentar
    okayyy

--- PIS-vj1/PIS-vj1/Program.cs
+++ PIS-vj1/PIS-vj1/Program.cs
@@ -9,7 +9,7 @@ namespace PIS_vj1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("YO");
+            Console.WriteLine("save as new revision");
         }
         
     }
